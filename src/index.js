70ac34155dadaf1d6c52ad2d68e6d110f83f523e
Ken Luo <luozhirong@gmail.com>
2017-06-06 08:55:47 -0700

    finished the tic tac toe

--- src/index.js
+++ src/index.js
@@ -49,10 +49,19 @@ class Game extends React.Component {
                 squares: Array(9).fill(null),
             }],
             xIsNext: true,
+            stepNumber: 0,
         };
     }
+
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) ? false : true,
+        });
+    }
+
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
@@ -65,18 +74,19 @@ class Game extends React.Component {
                 squares: squares
             }]),
             xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
             const desc = move ? 'Move # ' + move : 'Game start';
             return (
-                <li>
+                <li key={move}>
                     <a href="x" onClick={() => this.jumpTo(move)} > {desc} </a>
                 </li>
             );
